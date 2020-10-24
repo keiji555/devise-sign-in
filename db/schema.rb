39596230c80e2b74f830b46bf9910f5929c236c3
keiji <calling.cat.sw@gmail.com>
2020-10-24 22:44:28 +0900

    マイグレーションファイル修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_23_172934) do
+ActiveRecord::Schema.define(version: 2020_10_24_134259) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
